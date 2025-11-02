7feb3b3dd569e9300e1c5bd5d7603539614d09df
Samarsh7520 <samarsh.jaiz@gmail.com>
2025-11-02 17:49:24 +0530

    Add files via upload

---
+++ Homework.py
@@ -0,0 +1,10 @@
+season = input("Enter season: summer/winter/rainy: ")
+
+if season == "summer":
+    print("You can wear light clothes and wear sunscreen")
+
+elif season == "rainy":
+    print("You can take an umbrella and a raincoat every time you go outside")
+
+else:
+    print("You can wear Woolen clothes")
